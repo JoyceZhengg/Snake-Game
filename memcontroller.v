5f336cffb79878185893a92ae2ba3c6208fcfa14
nishkajhavar <nishka.a.jhavar@gmail.com>
2026-04-23 00:26:23 -0500

    fixed timing issue wrt vga and mem controller and stuff

--- memcontroller.v
+++ memcontroller.v
@@ -29,8 +29,8 @@ module memcontroller(input clk,
 
     // cpu to vga frame buffer reads/writes
     assign vga_raddr = raddr1;
-    assign vga_wen = (wen && waddr >= 16'h8000 && waddr < 16'hcb00);
-    assign vga_waddr = waddr;
+    assign vga_wen   = wen;    // Use the raw write-enable from the CPU
+    assign vga_waddr = waddr;  // Use the raw write-back address
     assign vga_wdata = wdata[7:0];
 
     reg [15:0] raddr1_ = 0;
@@ -42,7 +42,7 @@ module memcontroller(input clk,
 
     assign rdata1 = raddr1__ < 16'h8000 ? data_out :
                     raddr1__ < 16'hcb00 ? {8'b0, vga_rdata} :
-                    raddr1__ == 16'd000 ? {12'b0, button_reg} : 16'b0; // 0 for undefined memory
+                    raddr1__ == 16'hd000 ? {12'b0, button_reg} : 16'b0; // 0 for undefined memory
 
 endmodule
 
