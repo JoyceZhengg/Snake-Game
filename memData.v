7bc07f55a06662998988f998e93b0f7f2f9485e6
nishkajhavar <nishka.a.jhavar@gmail.com>
2026-04-23 13:05:35 -0500

    mem data size of arr fix

--- memData.v
+++ memData.v
@@ -1,27 +1,26 @@
 `timescale 1ns/1ps
 
-module memData(input clk,
-    input [15:0]raddr_, output [15:0]rdata_,
-    input wen, input [15:0]waddr, input [15:0]wdata);
+module memData(
+    input wire clk,
+    input wire [15:0] raddr,
+    output reg [15:0] rdata,
+    input wire wen,
+    input wire [15:0] waddr,
+    input wire [15:0] wdata
+);
+    // Upgraded from 8-bit to 16-bit wide memory!
+    reg [15:0] memory [0:16383]; 
 
-    reg [7:0]data[0:16'h7fff];
-
-    /* Simulation -- read initial content from file */
-    initial begin
-        $readmemb("memData.bin",data);
-    end
-
-    reg [15:0]raddr;
-    reg [15:0]rdata;
-
-    assign rdata_ = rdata;
+    // Convert the CPU's byte addresses (0x4000, 0x4002) 
+    // to internal word indexes (0x2000, 0x2001)
+    wire [13:0] safe_waddr = waddr[14:1];
+    wire [13:0] safe_raddr = raddr[14:1];
 
     always @(posedge clk) begin
-        raddr <= raddr_;
-        rdata <= {data[raddr]};
         if (wen) begin
-            data[waddr] <= wdata;
+            memory[safe_waddr] <= wdata;
         end
+        rdata <= memory[safe_raddr];
     end
 
 endmodule
