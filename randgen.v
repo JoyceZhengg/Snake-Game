c5f780343bea04368cfd8ed30fa4ad2eacbea18c
jacksone1 <jackson.b.edwards@gmail.com>
2026-04-22 22:25:05 -0500

    Fixed randgen typo, added buffertest.bin

--- randgen.v
+++ randgen.v
@@ -5,7 +5,7 @@ module randgen(input clk, output[15:0]randnum_);
     LFSR11 LFSR11(clk, bitA);
     LFSR17 LFSR17(clk, bitB);
     LFSR19 LFSR19(clk, bitC);
-    reg [13:0]randnum = 14'b010111 01110011;
+    reg [13:0]randnum = 14'b01011101110011;
 
     assign randbit = bitC ? bitA : bitB;
     assign randnum_ = {2'b0, randnum};
